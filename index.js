9a8523ab7df1f4b4a95460b1a25dc006d68fc81b
dexterous17 <dexterousprajapati@gmail.com>
2021-08-05 17:18:26 -0400

    ejs installed, Changes has been made to index.html and changes has been made to inde.js

--- index.js
+++ index.js
@@ -4,37 +4,51 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser'); 
 
 
+
 app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.engine('html', require('ejs').renderFile);
 
+
+app.get('/',(req,res)=>{
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(ip);
+  res.render('index.html')
+})
 
 app.post('/sendemail',(req,res)=>{
-  const data = req.body;
-  console.log(data)
-  /*var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'youremail@gmail.com',
-      pass: 'yourpassword'
-    }
-  });
-  
-  var mailOptions = {
-    from: 'youremail@gmail.com',
-    to: 'myfriend@yahoo.com',
-    subject: 'Sending Email using Node.js',
-    html: 'That was easy!'
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });*/
 
+  const Email  =req.body.Email;
+  const Password = req.body.Password;
+  const To = req.body.To;
+  const Subject = req.body.Subject;
+  const Html = req.body.Html;
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: Email,
+        pass: Password
+         }
+    });
+    
+  
+    const mailOptions = {
+      from: Email,
+      to: To,
+      subject: Subject,
+      html: Html + '<image src="" alt="image"></image>'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        res.redirect('/')
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.sendStatus(200)
+      }
+    });
 })
 
 
@@ -46,4 +60,4 @@ app.use('/images', (req,res) => {
   pixelBytes.write("R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=", "base64");
   res.send(pixelBytes, { 'Content-Type': 'image/gif' }, 200);
 });
-app.listen(5000);
+app.listen(process.env.port || 5000);
